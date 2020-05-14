45a3a36c7c3d644290bd11386a993ad258b8acd9
Surendar Vinayagamoorthy <svinayagamoorthy@dminc.com>
2020-05-14 11:53:48 +0530

    initial commit

---
+++ code/001_destructuring.js
@@ -0,0 +1,18 @@
+const person = {
+    name: 'John Doe',
+    age: 25,
+    gender: 'male'
+}
+
+// Conventional Method
+const conventionalName = person.name;
+const conventionalAge = person.age;
+const conventionalGender = person.gender;
+
+console.log(`${conventionalName}, ${conventionalAge}, ${conventionalGender}`);
+// John Doe, 25, male
+
+// Object Destructuring
+const { name, age, gender } = person;
+console.log(`${name}, ${age}, ${gender}`);
+// John Doe, 25, male
