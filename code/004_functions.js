2c69f6d956558c8a8529339e22b193d67beee188
Surendar Vinayagamoorthy <svinayagamoorthy@dminc.com>
2020-05-16 12:16:37 +0530

    day 4 - functions

---
+++ code/004_functions.js
@@ -0,0 +1,28 @@
+// Function Implementations
+
+// Conventional Function
+function sum(a, b) {
+    return a + b;
+}
+
+// Function in a variable
+const sum = function(a, b) {
+    console.log('here');
+    return a + b;
+}
+
+// Arrow Functions
+const sum = (a, b) => {
+    return a + b;
+}
+
+// Arrow function without return keyword
+const sum = (a, b) => a + b;
+
+// Function with a default param
+const sum = (a, b = 0) => a + b;
+
+// Using Function constructor
+const sum = new Function('a', 'b', 'return a + b');
+
+// console.log(sum(1, 4)); // 5
