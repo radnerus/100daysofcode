85217eec85a5db26f47c9a8474b0867b6ccef89d
radnerus <surendarandyou@gmail.com>
2020-06-03 10:02:35 +0530

    added snippets for da 16

--- code/005_spread.js
+++ code/005_spread.js
@@ -21,9 +21,24 @@ person.name = 'Doe';
 console.log(copiedPerson); // { name: 'Doe' }
 console.log(copiedPersonUsingSpread); // { name: 'John' }
 
+const sourceArray = [1, 2, 3, 4];
+const copiedArray = sourceArray;
+const copiedArrayUsingSpread = [...sourceArray];
+
+sourceArray.pop();
+
+console.log(copiedArray); // [ 1, 2, 3 ]
+console.log(copiedArrayUsingSpread); // [ 1, 2, 3, 4 ]
+
 // Works with Array too
-const copiedArray = [...joinedArray];
+// const copiedArray = [...joinedArray];
 
 // Works as param in function
 const numbers = [1, 5, 7, 2, 3, 4, 5, 7, 90];
 console.log(Math.max(...numbers)); // 90
+
+const multiplyBy = (multiplier, ...numbers) => {
+  console.log(numbers.map((num) => num * multiplier));
+};
+
+multiplyBy(2, 1, 2, 3, 4, 5, 6); // [ 2, 4, 6, 8, 10, 12 ]
