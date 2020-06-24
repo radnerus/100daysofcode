66433aa6b7b0dd4c062364c5375ef208bcbd1272
radnerus <surendarandyou@gmail.com>
2020-06-24 10:29:47 +0530

    Added code on day 36 - edited

--- code/036_range_generator.js
+++ code/036_range_generator.js
@@ -2,5 +2,5 @@ const generateTill = (n) => {
   return new Array(n).fill().map((_, _index) => _index + 1);
 };
 
-console.log(generateTill(5)); //[ 1, 2, 3, 4,  5, 6, 7, 8, 9, 10 ]
-console.log(generateTill(10)); //[ 1, 2, 3, 4,  5, 6, 7, 8, 9, 10 ]
+console.log(generateTill(5)); //[ 1, 2, 3, 4, 5 ]
+console.log(generateTill(10)); //[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
