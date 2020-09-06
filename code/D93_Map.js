87f852b724937a6a9b719e82eb9fc31f0a399cd3
radnerus <surendarandyou@gmail.com>
2020-09-06 15:32:03 +0530

    Added code for map

---
+++ code/D93_Map.js
@@ -0,0 +1,77 @@
+
+const products = [
+    {
+        name: 'SIEVE BOX',
+        image: 'images/gallery/SieveBox.jpeg'
+    },
+    {
+        name: 'SIEVE BOX FRONT PLATE aka TRUE PLATE',
+        image: 'images/gallery/SieveBox-FrontPlate.jpeg'
+    },
+    {
+        name: 'BEATER BAR SET',
+        image: 'images/gallery/BeaterBar.jpeg'
+    },
+    {
+        name: '96 BAR',
+        image: 'images/gallery/96Bar.jpeg'
+    },
+    {
+        name: 'WRAP GUARD SS',
+        image: 'images/gallery/WrapGuard.jpeg'
+    },
+    {
+        name: 'WRAP GUARD SS LATEST',
+        image: 'images/gallery/WrapGaurd-Latest.jpeg'
+    },
+    {
+        name: 'SS SEPARATOR GRATE aka JALLI - Small',
+        image: 'images/gallery/SeparatingGrateSmall.jpeg'
+    },
+    {
+        name: 'SS SEPARATOR GRATE aka JALLI - Medium',
+        image: 'images/gallery/SeparatingGrateMedium.jpeg'
+    },
+    {
+        name: 'SS SEPARATOR GRATE aka JALLI - Big',
+        image: 'images/gallery/SepratingGrate-Big.jpeg'
+    },
+    {
+        name: 'MOUTH PIECE SS',
+        image: 'images/gallery/MouthPiece.jpeg'
+    },
+    {
+        name: 'S-HOSE SMALL',
+        image: 'images/gallery/S-Hose-Small.jpeg'
+    },
+    {
+        name: 'S-HOSE BIG',
+        image: 'images/gallery/SHoseBig.jpeg'
+    },
+    {
+        name: 'CONNECTOR PIPE',
+        image: 'images/gallery/ConnectorPipe.jpeg'
+    },
+    {
+        name: 'ROTOR SPIKES aka ROTOR KOKIE',
+        image: 'images/gallery/RotorSpikes.jpeg'
+    },
+    {
+        name: 'STEP SHEET SMALL',
+        image: 'images/gallery/Stepsheet-small.jpeg'
+    },
+    {
+        name: 'STEP SHEET MEDIUM',
+        image: 'images/gallery/StepSheet-Medium.jpeg'
+    },
+    {
+        name: 'STEP SHEET BIG',
+        image: 'images/gallery/StepSheet-Big.jpeg'
+    },
+    {
+        name: 'Table Auger',
+        image: 'images/gallery/TableAuger.jpeg'
+    }
+];
+
+console.log(products.map(p => p.image));
